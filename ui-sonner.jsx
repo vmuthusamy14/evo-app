72ee3cc4df163d6c35c7fc90413847e2616eccb5
vmuthusamy14 <vaishnavi.jaymuthus@gmail.com>
2026-06-07 12:55:03 -0700

    Update ui-sonner.jsx

--- ui-sonner.jsx
+++ ui-sonner.jsx
@@ -1,21 +1,27 @@
-import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
+"use client";
+import { useTheme } from "next-themes"
+import { Toaster as Sonner } from "sonner"
 
-import { cn } from "@/lib/utils"
+const Toaster = ({
+  ...props
+}) => {
+  const { theme = "system" } = useTheme()
 
-const Slider = React.forwardRef(({ className, ...props }, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
-    className={cn("relative flex w-full touch-none select-none items-center", className)}
-    {...props}>
-    <SliderPrimitive.Track
-      className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb
-      className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
-  </SliderPrimitive.Root>
-))
-Slider.displayName = SliderPrimitive.Root.displayName
-
-export { Slider }
+  return (
+    (<Sonner
+      theme={theme}
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton:
+            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton:
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        },
+      }}
+      {...props} />)
+  );
+}
